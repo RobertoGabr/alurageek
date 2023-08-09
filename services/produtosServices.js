e4c6406b0c37fbccf7bd50cf27b5fbb2081bc7c2
Alura Geek <gabriel.santana479@gmail.com>
2023-08-09 17:51:45 -0300

    mockapi

--- services/produtosServices.js
+++ services/produtosServices.js
@@ -1,24 +1,27 @@
 //GET
 
 const listaProdutos = () => {
-  return fetch("http://localhost:3000/produtos").then((resposta) =>
-    resposta.json().catch((error) => console.log(error))
+  return fetch("https://64d3ed4667b2662bf3dcdfd2.mockapi.io/produto").then(
+    (resposta) => resposta.json().catch((error) => console.log(error))
   );
 };
 
 async function criaVideo(imagem, name, preco, descricao) {
-  const conexao = await fetch("http://localhost:3000/produtos", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({
-      imagem: imagem,
-      name: name,
-      preco: preco,
-      descricao: descricao,
-    }),
-  });
+  const conexao = await fetch(
+    "https://64d3ed4667b2662bf3dcdfd2.mockapi.io/produto",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        imagem: imagem,
+        name: name,
+        preco: preco,
+        descricao: descricao,
+      }),
+    }
+  );
 
   const conexaoConvertida = await conexao.json();
 
